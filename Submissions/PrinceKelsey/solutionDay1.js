a30929fe47b2dc4206d4348e5ba3c52c138f498d
Prince Ajuobi <princekelsey299@gmail.com>
2019-02-25 17:13:30 +0200

    solution day 1

---
+++ Submissions/PrinceKelsey/solutionDay1.js
@@ -0,0 +1,28 @@
+
+// Using a foorloop
+function longestWord(sentence) {
+    var str = sentence.split(" ");
+    var longest = 0;
+    var word = null;
+    for (var i = 0; i < str.length; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
+}
+
+// Using a foEach
+function longestWord(sentence) {
+    var str = sentence.split(" ");
+    var longest = 0;
+    var word = null;
+    str.forEach(function(str) {
+        if (longest < str.length) {
+            longest = str.length;
+            word = str;
+        }
+    });
+    return word;
+}
